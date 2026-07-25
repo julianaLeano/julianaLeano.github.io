d154514c7152a05454bdab5a980a075f46e5c5aa
julianaLeano <julexd125@gmail.com>
2026-07-25 13:06:19 -0500

    missed a few lol

---
+++ script.js
@@ -0,0 +1,56 @@
+const images = document.querySelectorAll(".gallery-img");
+
+const modal = document.getElementById("lightbox");
+const modalImg = document.getElementById("modal-img");
+const title = document.getElementById("modal-title");
+const description = document.getElementById("modal-descr");
+const refs = document.getElementById("modal-refs");
+const modalVideo = document.getElementById("modal-video");
+
+const closeBtn = document.getElementById("close-btn");
+
+images.forEach(image => {
+
+    image.addEventListener("click", () => {
+
+        modal.style.display = "flex";
+        image.style.border = ".75rem solid var(--primary-color)";
+
+        // modalImg.src = image.src;
+        // modalImg.alt = image.alt;
+        modalVideo.src = `https://www.youtube.com/embed/${image.dataset.video}`;
+        
+        title.textContent = image.alt;
+        description.textContent = image.dataset.descr;
+        
+        // clear old references just in case I guess
+        refs.innerHTML = "";
+        
+        // Get references from data attribute
+        const referenceList = JSON.parse(image.dataset.refs);
+        
+        // Create the bullet list
+        const ul = document.createElement("ul");
+        
+        referenceList.forEach(reference => {
+            const li = document.createElement("li");
+            const link = document.createElement("a");
+    
+            link.textContent = reference.name;
+            link.href = reference.url;
+            link.target = "_blank";
+    
+            li.appendChild(link);
+            ul.appendChild(li);
+        });
+        
+        refs.appendChild(ul);
+    });
+    
+});
+
+closeBtn.addEventListener("click", () => {
+    modal.style.display = "none";
+    modalVideo.src = "";
+    images.forEach(image => {image.style.border = ".75rem solid var(--secondary-color)";});
+});
